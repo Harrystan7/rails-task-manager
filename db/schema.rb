52b14450dcead50783888fbd3becc6e0e8d8cd29
Harry Stanworth <harry_stan57@hotmail.com>
2022-05-25 17:33:01 +0400

    rails new

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_24_101301) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.string "boolean", default: "f"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
